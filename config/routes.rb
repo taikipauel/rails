2cde5852b5b52622d8e5d6befcc41d52324e9de7
taikinishimura <taiki.daigaku@outlook.jp>
2020-03-17 19:05:35 +0900

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
-  root "application#hello"
+  #「get」とあるので、GETリクエストを受け取った際のアクションを結び付けている
+  get 'static_pages/about'
+  get 'static_pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

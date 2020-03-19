fac81b5bd5484c83d898395a968589a2dd60c482
taikinishimura <taiki.daigaku@outlook.jp>
2020-03-19 13:02:38 +0900

    Finish Part 5. Test didn't work well. if you want test work well, restart here!

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
+  get '/help', to: 'static_pages#help', as: 'help'
   #「get」とあるので、GETリクエストを受け取った際のアクションを結び付けている
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

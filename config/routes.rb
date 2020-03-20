c543535ed7a1b36e05bf7ed5514f74a6b386226f
taikinishimura <taiki.daigaku@outlook.jp>
2020-03-20 19:03:42 +0900

    finish chapter7

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,10 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/signup', to: 'users#create' #restfulルーティングの名前を変更！
+  #リソースを扱うページでないので、上記は、リソースベースのルーティングをしていない
+
+  resources :users #左で、railsのrest機能がusersリソースに対して有効となる
+
 end
+
